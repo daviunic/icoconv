b70e466944b71633781582af3da02061528f9965
daviunic <simunic.david@outlook.com>
2019-04-12 12:57:06 +0200

    Version 1.1
    Added support for converting new format icons to old format
    Cleaned up the code

--- icoconv/Program.cs
+++ icoconv/Program.cs
@@ -7,52 +7,35 @@ namespace icoconv
     {
         static void Main(string[] args)
         {
-            if (args.Length < 1)
+            if (args.Length < 2)
             {
-                Console.WriteLine("ICO Converter version 1.0\nCopyright (c) 2019 David Simunič. All rights reserved.\n\nNo arguments given. Use /? for help.");
+                Console.WriteLine("ICO Converter version 1.1\nCopyright (c) 2019 David Simunič. All rights reserved.\n\nUsage: ICOCONV.EXE <input filename> <output filename>");
                 Environment.Exit(0);
             }
 
-            //The help switch
-            if(args.Length == 1 && args[0] == "/?")
+            string input = args[0]; //Input filename
+            string output = args[1]; //Output filename
+            short firstWord; //The first word in the file
+
+            //Read the first word to determine the format of the input file
+            using (BinaryReader b = new BinaryReader(File.Open(input, FileMode.Open)))
             {
-                Console.WriteLine("ICO Converter version 1.0\nCopyright (c) 2019 David Simunič. All rights reserved.\n\n" +
-                    "Usage: ICOCONV.EXE <options> <input filename> <output filename>\n\nAvailable options:\n" +
-                    "  /? : displays this message");
-                Environment.Exit(0);
+                firstWord = b.ReadInt16();
             }
 
-            if(args.Length == 2)
+            if (firstWord == 1) //Input file is in old format, converting to new
             {
-                string input = args[0]; //Input filename
-                string output = args[1]; //Output filename
-                short firstWord; //The first word in the file
-
-                //Read the first word to determine the format of the input file
-                using (BinaryReader b = new BinaryReader(File.Open(input, FileMode.Open)))
-                {
-                    firstWord = b.ReadInt16();
-                    //Console.WriteLine("firstWord: " + firstWord);
-                }
-
-                if (firstWord == 1) //Input file is in old format, converting to new
-                {
-                    OldToNew(input, output);
-                }
-                else if (firstWord == 0) //Input file is in new format, converting to old
-                {
-                    NewToOld(input, output);
-                }
+                IcoOldToNew(input, output);
+            }
+            else if (firstWord == 0) //Input file is in new format, converting to old
+            {
+                IcoNewToOld(input, output);
             }
         }
 
-        //Converts icon in old format to new format
-        static void OldToNew(string input, string output)
+        //Converts icon in old format to new format - THIS ASSUMES A DEVICE-INDEPENDENT OLD ICON THAT'S 64x64 IN SIZE AND MONOCHROME!
+        static void IcoOldToNew(string input, string output)
         {
-            short widthPixels; //Width of the bitmap in pixels
-            short heightPixels; //Height of the bitmap in pixels
-            short widthBytes; //Width of the bitmap in bytes
-
             byte[] firstBitmap; //The first bitmap
             byte[] secondBitmap; //The second bitmap
             byte[] newFirstBitmap = new byte[512]; //The new first bitmap (=vertically mirrored second bitmap)
@@ -66,34 +49,25 @@ namespace icoconv
                                 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 0xFF, 0xFF, 0xFF, 00};
 
             //The new ICO header
-            byte[] newHeader = {00, 00, 01, 00, 01, 00, 0x40, 0x40, 02, 00, 01, 00, 01, 00, 0x30, 04, 00, 00, 0x16, 00, 00, 00};
+            byte[] newHeader = { 00, 00, 01, 00, 01, 00, 0x40, 0x40, 02, 00, 01, 00, 01, 00, 0x30, 04, 00, 00, 0x16, 00, 00, 00 };
 
             using (BinaryReader b = new BinaryReader(File.Open(input, FileMode.Open)))
             {
-                int pos = 6; //At offset 6 is the word that contains the width of the bitmap in pixels
+                int pos = 0xE; //At offset 0xE is the first bitmap
                 b.BaseStream.Seek(pos, SeekOrigin.Begin);
-
-                //Read the relevant information (icon dimensions) from the header
-                widthPixels = b.ReadInt16();
-                heightPixels = b.ReadInt16();
-                widthBytes = b.ReadInt16();
-
-                b.ReadInt16(); //Advance the stream to the start of the first bitmap
 
                 //Read each bitmap into its own byte array
                 firstBitmap = b.ReadBytes(512);
                 secondBitmap = b.ReadBytes(512);
             }
 
-            //Console.WriteLine("widthPixels: " + widthPixels + ", heightPixels: " + heightPixels + ", widthBytes: " + widthBytes);
-
             //Reverse the row order for each bitmap (aka mirror the bitmap vertically)
             for (int i = 0; i < 64; i++)
             {
-                for(int j = 0; j < 8; j++)
+                for (int j = 0; j < 8; j++)
                 {
-                    newFirstBitmap[504-i*8+j] = secondBitmap[i*8+j];
-                    newSecondBitmap[504-i*8+j] = firstBitmap[i*8+j];
+                    newFirstBitmap[504 - i * 8 + j] = secondBitmap[i * 8 + j];
+                    newSecondBitmap[504 - i * 8 + j] = firstBitmap[i * 8 + j];
                 }
             }
 
@@ -110,10 +84,48 @@ namespace icoconv
             Console.ReadLine();
         }
 
-        //Converts icon in new format to old format
-        static void NewToOld(string input, string output)
+        //Converts icon in new format to old format - THIS ASSUMES A SINGLE IMAGE 64x64 MONOCHROME NEW FORMAT ICON!
+        static void IcoNewToOld(string input, string output)
         {
+            byte[] firstBitmap; //The first bitmap
+            byte[] secondBitmap; //The second bitmap
+            byte[] newFirstBitmap = new byte[512]; //The new first bitmap (=vertically mirrored second bitmap)
+            byte[] newSecondBitmap = new byte[512]; //The new second bitmap (=vertically mirrored first bitmap)
 
+            //The following header is currently hardcoded, though I don't imagine there's much variety possible here considering the limitations
+            //of the old ICO format.
+            byte[] oldHeader = { 01, 00, 00, 00, 00, 00, 0x40, 00, 0x40, 00, 0x08, 00, 00, 00 };
+
+            using (BinaryReader b = new BinaryReader(File.Open(input, FileMode.Open)))
+            {
+                int pos = 0x46; //At offset 0x46 is the first bitmap
+                b.BaseStream.Seek(pos, SeekOrigin.Begin);
+
+                //Read each bitmap into its own byte array
+                firstBitmap = b.ReadBytes(512);
+                secondBitmap = b.ReadBytes(512);
+            }
+
+            //Reverse the row order for each bitmap (aka mirror the bitmap vertically)
+            for (int i = 0; i < 64; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    newFirstBitmap[504 - i * 8 + j] = secondBitmap[i * 8 + j];
+                    newSecondBitmap[504 - i * 8 + j] = firstBitmap[i * 8 + j];
+                }
+            }
+
+            //Write the new data to the output file in new format
+            using (BinaryWriter b = new BinaryWriter(File.Open(output, FileMode.Create)))
+            {
+                b.Write(oldHeader);
+                b.Write(newFirstBitmap);
+                b.Write(newSecondBitmap);
+            }
+
+            Console.WriteLine("New format icon " + input + " has been successfully converted to old format icon " + output + ".");
+            Console.ReadLine();
         }
     }
 }
